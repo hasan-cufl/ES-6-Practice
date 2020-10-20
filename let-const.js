38b16ce1adb8aa2c9cf223d543194d355d10a1a7
hasan-cugl <faridulhasancufl@gmail.com>
2020-10-20 19:06:44 +0600

    let-const practice

---
+++ let-const.js
@@ -0,0 +1,22 @@
+const hubby = "Omor sunny";
+console.log(hubby);
+
+const numbers = [12, 23, 64];
+numbers[1] = 34;
+numbers.push(56);
+console.log(numbers)
+
+const person = {name: "habib", address: "kolabagan", phone: 5798};
+
+
+let patientName = "kamal";
+patientName = "jamal";
+patientName = "hasina";
+console.log(patientName)
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i)
