875054615569e8f77fbc4161eb20344de9cb4832
hasan-cugl <faridulhasancufl@gmail.com>
2020-10-22 15:50:15 +0600

    destructure

--- destructure.js
+++ destructure.js
@@ -2,18 +2,18 @@
 // Destructure, Object, array, destructure complex object
 const person = {name: 'Jack willam', age: 17, job: 'facebooking', gfName: 'Ema watson', address: 'New york', phone: 46765, friends: ['Tom hancks','Tom cruise', 'Tom yarn']}
 
-// old system
+//destructure old system
 // const gfName = person.gfName;
 // const phone = person.phone;
 
-// ES-6
+// destructure ES-6
 const { phone, gfName, address } = person;
 
 console.log(gfName,phone,address);
 console.log(gfName,phone,address);
 
 
-// complex object
+//destructure complex object
 const complexObject = {
     name: 'jarin',
     info:{
@@ -24,3 +24,9 @@ const complexObject = {
 
 const {address} = complexObject.info;
 console.log(address);
+
+// array destructure
+const friends = ["hasan", "jamal", "kamal", "abir", "salman", "atiq", "aqib"]
+const [first, second, ...remaining = friends;
+
+console.log(first, second, remaining)
